303f6b515965e524ee14d64e6f9724de89ca3dc7
rasadov <raufasadov23@gmail.com>
2024-03-23 15:59:57 +0400

    changes in profile page

--- src/app/static/js/profile.js
+++ src/app/static/js/profile.js
@@ -1,16 +1,23 @@
-// body = document.querySelector('body');
-// body.style.backgroundColor = 'rgb(47, 47, 47)';
-// console.log(document.querySelector('nav'));
-// document.querySelector('nav').style.backgroundColor = 'transparent';
-// document.querySelector('nav').classList.remove('bg-dark')
+// This file is used to handle the profile page
 document.querySelector('footer').remove()
 
 cart = document.querySelector('.flex-container');
 
+if (cart.children.length === 0) {
+    document.querySelector('#empty').classList.remove('hidden');
+}
+
+var btn = document.querySelectorAll('.remove');
+
+
+
+
+
 function track(product_id) {
     let button = document.getElementById(product_id);
     button.parentElement.parentElement.parentElement.parentElement.remove();
-    if (cart.children.length == 0) {
+
+    if (cart.children.length === 0) {
         document.querySelector('#empty').classList.remove('hidden');
     }
 
